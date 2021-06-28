96d989e7dec2c661f766b57091100a434d96f408
Nicolas Pietro <ncjhsn7@gmail.com>
2021-06-28 19:17:41 -0300

    refactor

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>  // Para usar strings
+#include <string.h>
 
 #ifdef WIN32
-#include <windows.h>  // Apenas para Windows
+#include <windows.h>
 #endif
 
 #ifdef __APPLE__
@@ -11,30 +11,33 @@
 #include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #else
-#include <GL/gl.h>    // Funções da OpenGL
-#include <GL/glu.h>   // Funções da GLU
-#include <GL/glut.h>  // Funções da FreeGLUT
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
 #endif
 
-// SOIL é a biblioteca para leitura das imagens
 #include <SOIL.h>
 
-// Um pixel RGB (24 bits)
 typedef struct {
     unsigned char r, g, b;
 } RGB8;
 
-// Uma imagem RGB
 typedef struct {
     int width, height;
     RGB8 *img;
 } Img;
 
-// Protótipos
+typedef struct {
+    unsigned char r, g, b;
+} RGB;
+
+int energyCalculator(RGB previousPixel, RGB nextPixel, RGB topPixel,
+                     RGB lowerPixel);
+int accumulatedEnergy(int *energy, int maxWidth, int actualWidth, int i);
 void load(char *name, Img *pic);
 void uploadTexture();
-void seamcarve(int targetWidth);  // executa o algoritmo
-void freemem();  // limpa memória (caso tenha alocado dinamicamente)
+void seamcarve(int targetWidth);
+void freemem();
 
 // Funções da interface gráfica e OpenGL
 void init();
@@ -42,16 +45,10 @@ void draw();
 void keyboard(unsigned char key, int x, int y);
 void arrow_keys(int a_keys, int x, int y);
 
-// Largura e altura da janela
 int width, height;
-
-// Largura desejada (selecionável)
 int targetW;
 
-// Identificadores de textura
 GLuint tex[3];
-
-// As 3 imagens
 Img pic[3];
 Img *source;
 Img *mask;
@@ -70,25 +67,6 @@ void load(char *name, Img *pic) {
         exit(1);
     }
     printf("Load: %d x %d x %d\n", pic->width, pic->height, chan);
-}
-
-//
-// Implemente AQUI o seu algoritmo
-void seamcarve(int targetWidth) {
-    // Aplica o algoritmo e gera a saida em target->img...
-
-    RGB8(*ptr)
-    [target->width] = (RGB8(*)[target->width])target->img;
-
-    for (int y = 0; y < target->height; y++) {
-        for (int x = 0; x < targetW; x++) ptr[y][x].r = ptr[y][x].g = 255;
-        for (int x = targetW; x < target->width; x++)
-            ptr[y][x].r = ptr[y][x].g = 0;
-    }
-    // Chame uploadTexture a cada vez que mudar
-    // a imagem (pic[2])
-    uploadTexture();
-    glutPostRedisplay();
 }
 
 void freemem() {
@@ -110,15 +88,8 @@ int main(int argc, char **argv) {
         exit(1);
     }
     glutInit(&argc, argv);
-
-    // Define do modo de operacao da GLUT
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 
-    // pic[0] -> imagem original
-    // pic[1] -> máscara desejada
-    // pic[2] -> resultado do algoritmo
-
-    // Carrega as duas imagens
     load(argv[1], &pic[0]);
     load(argv[2], &pic[1]);
 
@@ -127,40 +98,21 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    // A largura e altura da janela são calculadas de acordo com a maior
-    // dimensão de cada imagem
     width = pic[0].width;
     height = pic[0].height;
-
-    // A largura e altura da imagem de saída são iguais às da imagem original
-    // (1)
     pic[2].width = pic[1].width;
     pic[2].height = pic[1].height;
-
-    // Ponteiros para as structs das imagens, para facilitar
     source = &pic[0];
     mask = &pic[1];
     target = &pic[2];
-
-    // Largura desejada inicialmente é a largura da janela
     targetW = target->width;
 
-    // Especifica o tamanho inicial em pixels da janela GLUT
     glutInitWindowSize(width, height);
-
-    // Cria a janela passando como argumento o titulo da mesma
     glutCreateWindow("Seam Carving");
-
-    // Registra a funcao callback de redesenho da janela de visualizacao
     glutDisplayFunc(draw);
-
-    // Registra a funcao callback para tratamento das teclas ASCII
     glutKeyboardFunc(keyboard);
-
-    // Registra a funcao callback para tratamento das setas
     glutSpecialFunc(arrow_keys);
 
-    // Cria texturas em memória a partir dos pixels das imagens
     tex[0] = SOIL_create_OGL_texture((unsigned char *)pic[0].img, pic[0].width,
                                      pic[0].height, SOIL_LOAD_RGB,
                                      SOIL_CREATE_NEW_ID, 0);
@@ -168,32 +120,23 @@ int main(int argc, char **argv) {
                                      pic[1].height, SOIL_LOAD_RGB,
                                      SOIL_CREATE_NEW_ID, 0);
 
-    // Exibe as dimensões na tela, para conferência
     printf("Origem  : %s %d x %d\n", argv[1], pic[0].width, pic[0].height);
     printf("Máscara : %s %d x %d\n", argv[2], pic[1].width, pic[0].height);
-    sel = 0;  // pic1
+    sel = 0;
 
-    // Define a janela de visualizacao 2D
     glMatrixMode(GL_PROJECTION);
     gluOrtho2D(0.0, width, height, 0.0);
     glMatrixMode(GL_MODELVIEW);
 
-    // Aloca memória para a imagem de saída
-    pic[2].img =
-        malloc(pic[1].width * pic[1].height * 3);  // W x H x 3 bytes (RGB)
-    // Pinta a imagem resultante de preto!
+    pic[2].img = malloc(pic[1].width * pic[1].height * 3);  // W x H x 3 bytes (RGB)
     memset(pic[2].img, 0, width * height * 3);
-
-    // Cria textura para a imagem de saída
     tex[2] = SOIL_create_OGL_texture((unsigned char *)pic[2].img, pic[2].width,
                                      pic[2].height, SOIL_LOAD_RGB,
                                      SOIL_CREATE_NEW_ID, 0);
 
-    // Entra no loop de eventos, não retorna
     glutMainLoop();
 }
 
-// Gerencia eventos de teclado
 void keyboard(unsigned char key, int x, int y) {
     if (key == 27) {
         // ESC: libera memória e finaliza
@@ -201,8 +144,8 @@ void keyboard(unsigned char key, int x, int y) {
         exit(1);
     }
     if (key >= '1' && key <= '3')
-        // 1-3: seleciona a imagem correspondente (origem, máscara e resultado)
         sel = key - '1';
+
     if (key == 's') {
         seamcarve(targetW);
     }
@@ -223,8 +166,20 @@ void arrow_keys(int a_keys, int x, int y) {
             break;
     }
 }
-// Faz upload da imagem para a textura,
-// de forma a exibi-la na tela
+
+void seamcarve(int targetWidth) {
+    // RGB8(*ptr)[target->width] = (RGB8(*)[target->width])target->img;
+
+    // for (int y = 0; y < target->height; y++) {
+    //     for (int x = 0; x < targetW; x++) ptr[y][x].r = ptr[y][x].g = 255;
+    //     for (int x = targetW; x < target->width; x++)
+    //         ptr[y][x].r = ptr[y][x].g = 0;
+    // }
+
+    uploadTexture();
+    glutPostRedisplay();
+}
+
 void uploadTexture() {
     glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, tex[2]);
@@ -233,36 +188,22 @@ void uploadTexture() {
     glDisable(GL_TEXTURE_2D);
 }
 
-// Callback de redesenho da tela
 void draw() {
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  // Preto
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    // Para outras cores, veja exemplos em /etc/X11/rgb.txt
-
-    glColor3ub(255, 255, 255);  // branco
-
-    // Ativa a textura corresponde à imagem desejada
+    glColor3ub(255, 255, 255);
     glBindTexture(GL_TEXTURE_2D, tex[sel]);
-    // E desenha um retângulo que ocupa toda a tela
     glEnable(GL_TEXTURE_2D);
     glBegin(GL_QUADS);
-
     glTexCoord2f(0, 0);
     glVertex2f(0, 0);
-
     glTexCoord2f(1, 0);
     glVertex2f(pic[sel].width, 0);
-
     glTexCoord2f(1, 1);
     glVertex2f(pic[sel].width, pic[sel].height);
-
     glTexCoord2f(0, 1);
     glVertex2f(0, pic[sel].height);
-
     glEnd();
     glDisable(GL_TEXTURE_2D);
-
-    // Exibe a imagem
     glutSwapBuffers();
 }
