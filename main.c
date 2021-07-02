38ef05e01bd2e7d85fa41bc92a470849acbf0dd7
Nicolas Pietro <ncjhsn7@gmail.com>
2021-07-01 23:19:51 -0300

    seamcarving working but still have bugs

--- main.c
+++ main.c
@@ -17,6 +17,7 @@
 #endif
 
 #include <SOIL.h>
+#include <math.h>
 
 typedef struct {
   unsigned char r, g, b;
@@ -27,19 +28,16 @@ typedef struct {
   RGB8* img;
 } Img;
 
-typedef struct {
-  unsigned char r, g, b;
-} RGB;
-
-int energyCalculator(RGB previousPixel,
-                     RGB nextPixel,
-                     RGB topPixel,
-                     RGB lowerPixel);
-int sumEnergy(int* energy, int maxWidth, int actualWidth, int i);
+int energyCalculator(RGB8 previousPixel,
+                     RGB8 nextPixel,
+                     RGB8 topPixel,
+                     RGB8 lowerPixel);
+int accumulatedEnergy(int* energy, int maxWidth, int actualWidth, int i);
 void load(char* name, Img* pic);
 void uploadTexture();
-void seamcarve(int targetWidth);
+void seamcarve();
 void freemem();
+void removeLine(int direction);
 
 // Funções da interface gráfica e OpenGL
 void init();
@@ -49,7 +47,7 @@ void arrow_keys(int a_keys, int x, int y);
 
 int width, height;
 int targetW = 400;
-
+int dw = 10;
 GLuint tex[3];
 Img pic[3];
 Img* source;
@@ -141,6 +139,7 @@ int main(int argc, char** argv) {
 }
 
 void keyboard(unsigned char key, int x, int y) {
+  // int auxWidth = pic[2].width;
   if (key == 27) {
     // ESC: libera memória e finaliza
     freemem();
@@ -150,34 +149,360 @@ void keyboard(unsigned char key, int x, int y) {
     sel = key - '1';
 
   if (key == 's') {
-    seamcarve(targetW);
+    seamcarve();
   }
   glutPostRedisplay();
 }
 
-void seamcarve(int targetWidth) {
-  // RGB8(*ptr)[target->width] = (RGB8(*)[target->width])target->img;
+void arrow_keys(int a_keys, int x, int y) {
+  switch (a_keys) {
+    case GLUT_KEY_RIGHT:
+      // if (targetW <= pic[2].width - 10)
+      dw = 10;
+      removeLine(dw);
+      dw = 0;
+      break;
+    case GLUT_KEY_LEFT:
+      // if (targetW > 10)
+      // targetW -= 10;
+      dw = -10;
+      removeLine(dw);
+      dw = 0;
+      break;
+    default:
+      break;
+  }
+}
 
-  // for (int y = 0; y < target->height; y++) {
-  //     for (int x = 0; x < targetW; x++) ptr[y][x].r = ptr[y][x].g = 255;
-  //     for (int x = targetW; x < target->width; x++)
-  //         ptr[y][x].r = ptr[y][x].g = 0;
-  // }
+void removeLine(int direction) {
   int auxWidth = pic[2].width;
-  int desiredwidth = targetWidth, lowestEnergyPixel;
-  RGB* mask = malloc(pic[1].width * pic[1].height * 3);
+  int lowestEnergyPixel = 0;
+  printf("TARGET W VALUE: %d \n", dw);
+  printf("PICTURE WIDTH: %d \n", pic[2].width);
+  // pic[2].width += direction;
+  // auxWidth += direction;
+  RGB8* maskAux = malloc(pic[1].width * pic[1].height * 3);
 
-  for (int i = 0; i < pic[2].height * pic[2].width; i++) {
+  for (int i = 0; i < pic[2].height * pic[2].width; i++)
+    pic[2].img[i] = pic[0].img[i];
+
+  int* energy = malloc(sizeof(int) * pic[2].height * pic[2].width);
+
+  for (int i = 0; i < pic[2].height; i++) {
+    for (int j = 0; j < pic[2].width; j++) {
+      if (pic[1].img[(i * pic[2].width) + j].r > 200 &&
+          pic[1].img[(i * pic[2].width) + j].g < 100 &&
+          pic[1].img[(i * pic[2].width) + j].b < 100)
+        energy[(i * pic[2].width) + j] = -10000000;
+
+      else if (pic[1].img[(i * pic[2].width) + j].r < 100 &&
+               pic[1].img[(i * pic[2].width) + j].g > 200 &&
+               pic[1].img[(i * pic[2].width) + j].b < 100)
+        energy[(i * pic[2].width) + j] = 10000000;
+
+      else
+        energy[(i * pic[2].width) + j] = 0;
+
+      if (i == 0) {
+        if (j == 0)
+          energy[0] += energyCalculator(pic[2].img[1], pic[2].img[auxWidth - 1], pic[2].img[pic[2].width * (pic[2].height - 1)],
+                               pic[2].img[pic[2].width]);
+
+        else if (j == auxWidth - 1)
+          energy[j] += energyCalculator(
+              pic[2].img[auxWidth - 2], pic[2].img[0],
+              pic[2].img[(pic[2].width * pic[2].height - 1) + auxWidth - 1],
+              pic[2].img[(pic[2].width + auxWidth) - 1]);
+
+        else
+          energy[j] += energyCalculator(
+              pic[2].img[j - 1], pic[2].img[j + 1],
+              pic[2].img[j + (pic[2].width * (pic[2].height - 1))],
+              pic[2].img[j + pic[2].width]);
+
+      } else if ((i * pic[2].width) + j >= pic[2].width * (pic[2].height - 1)) {
+        if (j == 0) {
+          energy[i * pic[2].width] += energyCalculator(
+              pic[2].img[(pic[2].width * i) + auxWidth - 1],
+              pic[2].img[(i * pic[2].width) + j + 1],
+              pic[2].img[pic[2].width * (i - 1)], pic[2].img[0]);
+          energy[i * pic[2].width] += accumulatedEnergy(
+              energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+
+        }
+
+        else if (j == auxWidth - 1) {
+          energy[(i * pic[2].width) + j] += energyCalculator(
+              pic[2].img[(pic[2].width * i) + auxWidth - 2],
+              pic[2].img[pic[2].width * i],
+              pic[2].img[(pic[2].width * (i - 1)) + auxWidth - 1],
+              pic[2].img[j]);
+          energy[(i * pic[2].width) + j] += accumulatedEnergy(
+              energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+
+        } else {
+          energy[(i * pic[2].width) + j] += energyCalculator(
+              pic[2].img[(i * pic[2].width) + j - 1],
+              pic[2].img[(i * pic[2].width) + j + 1],
+              pic[2].img[((i - 1) * pic[2].width) + j], pic[2].img[j]);
+          energy[(i * pic[2].width) + j] += accumulatedEnergy(
+              energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+        }
+
+      }
+
+      else if (j == 0) {
+        energy[i * pic[2].width] +=
+            energyCalculator(pic[2].img[(i * pic[2].width) + auxWidth - 1],
+                             pic[2].img[(i * pic[2].width) + 1],
+                             pic[2].img[(i - 1) * pic[2].width],
+                             pic[2].img[(i + 1) * pic[2].width]);
+        energy[i * pic[2].width] += accumulatedEnergy(
+            energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+        ;
+
+      }
+
+      else if (j == auxWidth - 1) {
+        energy[(i * pic[2].width) + j] +=
+            energyCalculator(pic[2].img[(i * pic[2].width) + j - 1],
+                             pic[2].img[i * pic[2].width],
+                             pic[2].img[(i - 1) * pic[2].width + j],
+                             pic[2].img[(i + 1) * pic[2].width + j]);
+        energy[(i * pic[2].width) + j] += accumulatedEnergy(
+            energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+        ;
+
+      }
+
+      else {
+        energy[(i * pic[2].width) + j] +=
+            energyCalculator(pic[2].img[(i * pic[2].width) + j - 1],
+                             pic[2].img[(i * pic[2].width) + j + 1],
+                             pic[2].img[(i - 1) * pic[2].width + j],
+                             pic[2].img[(i + 1) * pic[2].width + j]);
+        energy[(i * pic[2].width) + j] += accumulatedEnergy(
+            energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+        ;
+      }
+    }
+  }
+
+  lowestEnergyPixel = pic[2].width * (pic[2].height - 1);
+
+  for (int i = lowestEnergyPixel;
+       i < pic[2].width * (pic[2].height - 1) + auxWidth; i++) {
+    if (energy[i] < energy[lowestEnergyPixel])
+      lowestEnergyPixel = i;
+  }
+
+  for (int i = lowestEnergyPixel;;) {
+    int column = i % pic[2].width;
+
+    for (int j = 0; j < auxWidth - column; j++) {
+      pic[2].img[i + j] = pic[2].img[i + j + 1];
+      pic[1].img[i + j] = pic[1].img[i + j + 1];
+    }
+
+    pic[2].img[(i - column) + auxWidth - 1].r = 0;
+    pic[2].img[(i - column) + auxWidth - 1].g = 0;
+    pic[2].img[(i - column) + auxWidth - 1].b = 0;
+
+    if (i - pic[2].width < 0)
+      break;
+
+    int lowestPath = i - pic[2].width;
+    int lowestEnergy = energy[i - pic[2].width];
+
+    if (i % pic[2].width != 0 && energy[i - pic[2].width - 1] < lowestEnergy) {
+      lowestEnergy = energy[i - pic[2].width - 1];
+      lowestPath = i - pic[2].width - 1;
+    }
+
+    if (i % pic[2].width != (auxWidth - 1) &&
+        energy[i - pic[2].width + 1] < lowestEnergy) {
+      lowestEnergy = energy[i - pic[2].width + 1];
+      lowestPath = i - pic[2].width + 1;
+    }
+
+    i = lowestPath;
+  }
+  free(energy);
+  uploadTexture();
+  
+  free(maskAux);
+}
+
+void seamcarve() {
+  int auxWidth = pic[2].width;
+  int desiredWidth = 400, lowestEnergyPixel;
+  RGB8* maskAux = malloc(pic[1].width * pic[1].height * 3);
+
+  for (int i = 0; i < pic[2].height * pic[2].width; i++)
+    pic[2].img[i] = pic[0].img[i];
+
+  while (auxWidth > desiredWidth) {
+    printf("(while) -> auxWidth: %d\n", auxWidth);
+    printf("(while) -> desiredWidth: %d\n", desiredWidth);
+    int* energy = malloc(sizeof(int) * pic[2].height * pic[2].width);
+
+    for (int i = 0; i < pic[2].height; i++) {
+      for (int j = 0; j < pic[2].width; j++) {
+        if (pic[1].img[(i * pic[2].width) + j].r > 200 &&
+            pic[1].img[(i * pic[2].width) + j].g < 100 &&
+            pic[1].img[(i * pic[2].width) + j].b < 100)
+          energy[(i * pic[2].width) + j] = -1000000;
+
+        else if (pic[1].img[(i * pic[2].width) + j].r < 100 &&
+                 pic[1].img[(i * pic[2].width) + j].g > 200 &&
+                 pic[1].img[(i * pic[2].width) + j].b < 100)
+          energy[(i * pic[2].width) + j] = 10000000;
+
+        else
+          energy[(i * pic[2].width) + j] = 0;
+
+        if (i == 0) {
+          if (j == 0)
+            energy[0] +=
+                energyCalculator(pic[2].img[1], pic[2].img[auxWidth - 1],
+                                 pic[2].img[pic[2].width * (pic[2].height - 1)],
+                                 pic[2].img[pic[2].width]);
+
+          else if (j == auxWidth - 1)
+            energy[j] += energyCalculator(
+                pic[2].img[auxWidth - 2], pic[2].img[0],
+                pic[2].img[(pic[2].width * pic[2].height - 1) + auxWidth - 1],
+                pic[2].img[(pic[2].width + auxWidth) - 1]);
+
+          else
+            energy[j] += energyCalculator(
+                pic[2].img[j - 1], pic[2].img[j + 1],
+                pic[2].img[j + (pic[2].width * (pic[2].height - 1))],
+                pic[2].img[j + pic[2].width]);
+
+        } else if ((i * pic[2].width) + j >=
+                   pic[2].width * (pic[2].height - 1)) {
+          if (j == 0) {
+            energy[i * pic[2].width] += energyCalculator(
+                pic[2].img[(pic[2].width * i) + auxWidth - 1],
+                pic[2].img[(i * pic[2].width) + j + 1],
+                pic[2].img[pic[2].width * (i - 1)], pic[2].img[0]);
+            energy[i * pic[2].width] += accumulatedEnergy(
+                energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+
+          }
+
+          else if (j == auxWidth - 1) {
+            energy[(i * pic[2].width) + j] += energyCalculator(
+                pic[2].img[(pic[2].width * i) + auxWidth - 2],
+                pic[2].img[pic[2].width * i],
+                pic[2].img[(pic[2].width * (i - 1)) + auxWidth - 1],
+                pic[2].img[j]);
+            energy[(i * pic[2].width) + j] += accumulatedEnergy(
+                energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+
+          } else {
+            energy[(i * pic[2].width) + j] += energyCalculator(
+                pic[2].img[(i * pic[2].width) + j - 1],
+                pic[2].img[(i * pic[2].width) + j + 1],
+                pic[2].img[((i - 1) * pic[2].width) + j], pic[2].img[j]);
+            energy[(i * pic[2].width) + j] += accumulatedEnergy(
+                energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+          }
+
+        }
+
+        else if (j == 0) {
+          energy[i * pic[2].width] +=
+              energyCalculator(pic[2].img[(i * pic[2].width) + auxWidth - 1],
+                               pic[2].img[(i * pic[2].width) + 1],
+                               pic[2].img[(i - 1) * pic[2].width],
+                               pic[2].img[(i + 1) * pic[2].width]);
+          energy[i * pic[2].width] += accumulatedEnergy(
+              energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+          ;
+
+        }
+
+        else if (j == auxWidth - 1) {
+          energy[(i * pic[2].width) + j] +=
+              energyCalculator(pic[2].img[(i * pic[2].width) + j - 1],
+                               pic[2].img[i * pic[2].width],
+                               pic[2].img[(i - 1) * pic[2].width + j],
+                               pic[2].img[(i + 1) * pic[2].width + j]);
+          energy[(i * pic[2].width) + j] += accumulatedEnergy(
+              energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+          ;
+
+        }
+
+        else {
+          energy[(i * pic[2].width) + j] +=
+              energyCalculator(pic[2].img[(i * pic[2].width) + j - 1],
+                               pic[2].img[(i * pic[2].width) + j + 1],
+                               pic[2].img[(i - 1) * pic[2].width + j],
+                               pic[2].img[(i + 1) * pic[2].width + j]);
+          energy[(i * pic[2].width) + j] += accumulatedEnergy(
+              energy, pic[2].width, auxWidth, (i * pic[2].width) + j);
+          ;
+        }
+      }
+    }
+
+    lowestEnergyPixel = pic[2].width * (pic[2].height - 1);
+
+    for (int i = lowestEnergyPixel;
+         i < pic[2].width * (pic[2].height - 1) + auxWidth; i++) {
+      if (energy[i] < energy[lowestEnergyPixel])
+        lowestEnergyPixel = i;
+    }
+
+    for (int i = lowestEnergyPixel;;) {
+      int column = i % pic[2].width;
+
+      for (int j = 0; j < auxWidth - column; j++) {
+        pic[2].img[i + j] = pic[2].img[i + j + 1];
+        pic[1].img[i + j] = pic[1].img[i + j + 1];
+      }
+
+      pic[2].img[(i - column) + auxWidth - 1].r = 0;
+      pic[2].img[(i - column) + auxWidth - 1].g = 0;
+      pic[2].img[(i - column) + auxWidth - 1].b = 0;
+
+      if (i - pic[2].width < 0)
+        break;
+
+      int lowestPath = i - pic[2].width;
+
+      int lowestEnergy = energy[i - pic[2].width];
+
+      if (i % pic[2].width != 0 &&
+          energy[i - pic[2].width - 1] < lowestEnergy) {
+        lowestEnergy = energy[i - pic[2].width - 1];
+        lowestPath = i - pic[2].width - 1;
+      }
+
+      if (i % pic[2].width != (auxWidth - 1) &&
+          energy[i - pic[2].width + 1] < lowestEnergy) {
+        lowestEnergy = energy[i - pic[2].width + 1];
+        lowestPath = i - pic[2].width + 1;
+      }
+
+      i = lowestPath;
+    }
+
+    free(energy);
+    auxWidth--;
   }
 
   uploadTexture();
-  glutPostRedisplay();
+  free(maskAux);
 }
 
-int energyCalculator(RGB previousPixel,
-                     RGB nextPixel,
-                     RGB topPixel,
-                     RGB lowerPixel) {
+int energyCalculator(RGB8 previousPixel,
+                     RGB8 nextPixel,
+                     RGB8 topPixel,
+                     RGB8 lowerPixel) {
   return (pow(previousPixel.r - nextPixel.r, 2)) +
          (pow(previousPixel.g - nextPixel.g, 2)) +
          (pow(previousPixel.b - nextPixel.b, 2)) +
@@ -189,35 +514,16 @@ int energyCalculator(RGB previousPixel,
          (pow(topPixel.b - lowerPixel.b, 2));
 }
 
-int sumEnergy(int* energy, int maxWidth, int actualWidth, int i) {
+int accumulatedEnergy(int* energy, int maxWidth, int actualWidth, int i) {
   int finalEnergy = energy[i - maxWidth];
-  if (i % maxWidth != 0 && energy[i - maxWidth - 1] < finalEnergy) {
+
+  if (i % maxWidth != 0 && energy[i - maxWidth - 1] < finalEnergy)
     finalEnergy = energy[i - maxWidth - 1];
-  }
 
-  if (i % maxWidth != (actualWidth - 1) &&
-      energy[i - maxWidth * 1] < finalEnergy) {
+  if (i % maxWidth != (actualWidth - 1) && energy[i - maxWidth * 1] < finalEnergy) 
     finalEnergy = energy[i - maxWidth * 1];
-  }
-
+  
   return finalEnergy;
-}
-
-void arrow_keys(int a_keys, int x, int y) {
-  switch (a_keys) {
-    case GLUT_KEY_RIGHT:
-      if (targetW <= pic[2].width - 10)
-        targetW += 10;
-      seamcarve(targetW);
-      break;
-    case GLUT_KEY_LEFT:
-      if (targetW > 10)
-        targetW -= 10;
-      seamcarve(targetW);
-      break;
-    default:
-      break;
-  }
 }
 
 void uploadTexture() {
